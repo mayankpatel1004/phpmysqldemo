c0ab2d01a26ef7634ccdcac4be80741d6cfa09ce
mayankpatel1004 <mayank.patel104@gmail.com>
2026-06-10 17:00:24 +0530

    Updates

--- src/templates/forgot-password.php
+++ src/templates/forgot-password.php
@@ -29,7 +29,7 @@
               <form class="pt-3" method="POST" id="forgot-password-form" action="<?php echo $site_url;?>routes?action=forgot-password">
                 <div class="form-group">
                   <input type="email" class="form-control form-control-lg" name="user_email" id="user_email"
-                    placeholder="Email" value="developer112018@yopmail.com" />
+                    placeholder="Email" value="" />
                 </div>
                 <div class="mt-3">
                   <button type="submit" id="submit_button"
