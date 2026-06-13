b3237cd45261b14bb09ae7ea07a3e7936528a8e9
mayankpatel1004 <mayank.patel104@gmail.com>
2026-06-13 18:18:14 +0530

    Updates

--- src/temp/temp.php
+++
@@ -1,3 +0,0 @@
-Array ( [status] => 1 [data] => Array ( [user_id] => 1 [site_id] => 1 [site_db] => nodejsframework [user_firstname] =>
-Developer [user_lastname] => Account [user_name] => developer [user_email] => developer112018@yopmail.com [user_role_id]
-=> 1 [is_developer_account] => Y [web_or_app] => Web [active_status] => Y [display_status] => Y ) )
